e117e1632e962be188654bd94321a1829a8a1a31
iopatich <iopatich@uade.edu.ar>
2026-04-13 13:17:36 -0300

    creacion de metodo de balance en clase Banco

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,2 +1,5 @@
 public class Banco {
+    public void mostrarBalanceCuentas(Usuario cuenta1, Usuario cuenta2, Usuario cuenta3) {
+        System.out.println("El balance total de las cuentas en el banco es de $" + (cuenta1.saldo + cuenta2.saldo + cuenta3.saldo));
+    }
 }
