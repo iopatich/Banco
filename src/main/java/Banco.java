4c422f084147b0ab7a5c1d2932a4717a9d6b1e3d
iopatich <iopatich@uade.edu.ar>
2026-04-13 14:51:32 -0300

    Feature/1304/Actualizacion en metodo en Banco

--- src/main/java/Banco.java
+++ src/main/java/Banco.java
@@ -1,5 +1,9 @@
 public class Banco {
     public void mostrarBalanceCuentas(Usuario cuenta1, Usuario cuenta2, Usuario cuenta3) {
+        System.out.println("El balance de cada cuenta es el siguiente");
+        System.out.println(cuenta1.getNombre() + ": " + cuenta1.getSaldo());
+        System.out.println(cuenta2.getNombre() + ": " + cuenta2.getSaldo());
+        System.out.println(cuenta3.getNombre() + ": " + cuenta3.getSaldo());
         System.out.println("El balance total de las cuentas en el banco es de $" + (cuenta1.getSaldo() + cuenta2.getSaldo() + cuenta3.getSaldo()));
     }
 }
