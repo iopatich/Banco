85a1d3a460016ff958e238575438422cc62a5d54
iopatich <iopatich@uade.edu.ar>
2026-04-14 17:55:48 -0300

    Feature/1404/Se aniade clase Cliente

---
+++ src/main/java/Cliente.java
@@ -0,0 +1,23 @@
+import java.util.ArrayList;
+
+public class Cliente {
+    private Usuario usuario;
+    private ArrayList<Cuenta> cuentas;
+
+    public Cliente(Usuario usuario) {
+        this.usuario = usuario;
+        this.cuentas = new ArrayList<>();
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public ArrayList<Cuenta> getCuentas() {
+        return cuentas;
+    }
+
+    public void agregarCuenta(Cuenta cuenta) {
+        cuentas.add(cuenta);
+    }
+}
